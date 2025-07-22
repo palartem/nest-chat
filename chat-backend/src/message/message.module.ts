ad999ee6c04dac036c04d4282079fdba322bb293
Artem Palashek <palartem@gmail.com>
2025-07-22 22:03:47 +0300

    - Проверка существует ли чат

--- chat-backend/src/message/message.module.ts
+++ chat-backend/src/message/message.module.ts
@@ -4,10 +4,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Message } from './message.entity';
 import { ChatModule } from '../chat/chat.module';
 import { UserModule } from '../user/user.module';
+import { MessageResolver } from './message.resolver';
 
 @Module({
     imports: [TypeOrmModule.forFeature([Message]), forwardRef(() => ChatModule), UserModule],
-    providers: [MessageService],
+    providers: [MessageService, MessageResolver],
     exports: [MessageService],
 })
 export class MessageModule {}
