052de1f2e72c148084f08b56df8c0a924e2a455a
Artem Palashek <palartem@gmail.com>
2025-08-11 22:36:51 +0300

    - Avatar user

---
+++ frontend/src/components/user/UserAvatar.vue
@@ -0,0 +1,46 @@
+<template>
+    <q-avatar :size="size" :color="color" :text-color="textColor">
+        {{ initial }}
+        <q-badge
+            v-if="isOnline"
+            color="green"
+            align="bottom"
+            rounded
+            floating
+            class="q-badge--online"
+        />
+    </q-avatar>
+</template>
+
+<script>
+export default {
+    name: 'UserAvatar',
+    props: {
+        name: {
+            type: String,
+            default: ''
+        },
+        isOnline: {
+            type: Boolean,
+            default: false
+        },
+        size: {
+            type: String,
+            default: '40px'
+        },
+        color: {
+            type: String,
+            default: 'primary'
+        },
+        textColor: {
+            type: String,
+            default: 'white'
+        },
+    },
+    computed: {
+        initial() {
+            return this.name?.trim()?.charAt(0)?.toUpperCase() || '?'
+        }
+    }
+}
+</script>
